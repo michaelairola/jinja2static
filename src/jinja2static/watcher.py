f6110fe29faffd3fccc92bab3c705d23c34b5c43
Michael Airola <me@michaelairola.com>
2026-01-05 18:28:40 -0800

    compress watcher a little for data file watching

--- src/jinja2static/watcher.py
+++ src/jinja2static/watcher.py
@@ -45,57 +45,36 @@ def detect_template_changes_build_index(file_path, config):
 def detect_changes_copy_asset(file_path, config):
     copy_asset_file(config, file_path.relative_to(config.assets))
 
-@watch_for_file_changes
-def detect_changes_yaml_data_files(file_path, config):
-    start_time = time.perf_counter()    
-    config.data_module.update_yaml_data()
-    effected_templates_dir = config.templates / config.data_module.relative_path
-    files_to_rebuild = list(effected_templates_dir.rglob("*"))
-    files_to_rebuild = [
-        page.relative_to(config.templates) for page in files_to_rebuild 
-        if page.relative_to(config.templates) in config.pages
-    ]
-    logger.info(f"Rebuilding {[str(file) for file in files_to_rebuild]}...")
-    for file_path in files_to_rebuild:
-        build_page(config, file_path)
-    end_time = time.perf_counter()
-    logger.info(f"Rebuilt in {(end_time - start_time):.4f} seconds")
-
-@watch_for_file_changes
-def detect_changes_pymod_data_files(file_path, config):
-    start_time = time.perf_counter()    
-    config.data_module.update_module_data()
-    effected_templates_dir = config.templates / config.data_module.relative_path
-    files_to_rebuild = list(effected_templates_dir.rglob("*"))
-    files_to_rebuild = [
-        page.relative_to(config.templates) for page in files_to_rebuild 
-        if page.relative_to(config.templates) in config.pages
-    ]
-    logger.info(f"Rebuilding {[str(file) for file in files_to_rebuild]}...")
-    for file_path in files_to_rebuild:
-        build_page(config, file_path)
-    end_time = time.perf_counter()
-    logger.info(f"Rebuilt in {(end_time - start_time):.4f} seconds")
+def detect_changes_data_files(file_path, config, callback_fn):
+    @watch_for_file_changes
+    def x(file_path, config):
+        start_time = time.perf_counter()    
+        config.data_module.update_module_data()
+        callback_fn()
+        effected_templates_dir = config.templates / config.data_module.relative_path
+        files_to_rebuild = list(effected_templates_dir.rglob("*"))
+        files_to_rebuild = [
+            page.relative_to(config.templates) for page in files_to_rebuild 
+            if page.relative_to(config.templates) in config.pages
+        ]
+        logger.info(f"Rebuilding {[str(file) for file in files_to_rebuild]}...")
+        for file_path in files_to_rebuild:
+            build_page(config, file_path)
+        end_time = time.perf_counter()
+        logger.info(f"Rebuilt in {(end_time - start_time):.4f} seconds")
+    return x(file_path, config)
 
 def file_watcher(config: Config):
     for file_path in config.templates.rglob("*"):
         create_task(detect_template_changes_build_index(file_path, config))
     for file_path in config.assets.rglob("*"):
         create_task(detect_changes_copy_asset(file_path, config))
-    
-    py_mod_file_path = config.data_module.python_module_file_path
-    if py_mod_file_path:
-        create_task(
-            detect_changes_pymod_data_files(
-                py_mod_file_path,
-                config
+
+    data_mod = config.data_module
+    files = [ data_mod.python_module_file_path, data_mod.yaml_file_path ]
+    callback_fns = [  data_mod.update_module_data, data_mod.update_yaml_data ]
+    for file_path, callback_fn in zip(files, callback_fns):
+        if file_path:
+            create_task(
+                detect_changes_data_files(file_path, config, callback_fn)
             )
-        )
-    yaml_file_path = config.data_module.yaml_file_path
-    if yaml_file_path:
-        create_task(
-            detect_changes_yaml_data_files(
-                yaml_file_path,
-                config
-            )
-        )
