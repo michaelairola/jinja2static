f6b9320ed9610bd9da2e660a38ded66dab8802d2
michaelairola <michael.airola@aero.org>
2026-01-06 15:02:58 -0800

    moved test repos to appropriate named dir

--- tests/test_build.py
+++ tests/test_build.py
@@ -6,14 +6,14 @@ from jinja2static.logger import configure_logging
 
 
 def test_build_resume():
-    RESUME_PATH = Path(__file__).parent / "data" / "resume"
+    RESUME_PATH = Path(__file__).parent / "test_repos" / "resume"
     configure_logging(True)
     config = Config.from_(RESUME_PATH)
     assert build(config)
 
 
 def test_build_blog():
-    BLOG_PATH = Path(__file__).parent / "data" / "blog"
+    BLOG_PATH = Path(__file__).parent / "test_repos" / "blog"
     configure_logging(True)
     config = Config.from_(BLOG_PATH)
     assert build(config)
